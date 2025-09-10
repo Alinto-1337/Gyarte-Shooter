34bfe627dca9b63b4ff9f69c1eaaa2199d87c4e1
Alinto <121405632+Alinto-1337@users.noreply.github.com>
2025-09-10 23:59:42 +0200

    InputManager

--- Assets/Scripts/Player/PlayerCharacterController.cs
+++ Assets/Scripts/Player/PlayerCharacterController.cs
@@ -1,18 +1,18 @@
 using System;
+using GyarteShooter.Input;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerCharacterController : MonoBehaviour
+public class PlayerCharacterController : MonoBehaviour, ProjectWideInputActions.IPlayerActions
 {
     [Tooltip("The object that will rotate with the mouse")]
     [SerializeField] Transform cameraRotationPivot;
 
-    [SerializeField] InputActionMap playerInputMap;
-
     void Start()
     {
-        
+        var inputActions = InputManager.Instance.inputActions;
+        inputActions.Player.Move.performed += OnMove;
     }
 
 
@@ -23,10 +23,55 @@ public class PlayerCharacterController : MonoBehaviour
 
     private void ApplyGravity()
     {
-        throw new NotImplementedException();
+        
     }
 
     private void ApplyMovement()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("character move");
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPrevious(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnNext(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
     {
         throw new NotImplementedException();
     }
---
+++ Assets/Scripts/Player/Test.cs
@@ -0,0 +1,64 @@
+using System;
+using GyarteShooter.Input;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Test : MonoBehaviour, ProjectWideInputActions.IPlayerActions
+{
+    [Tooltip("The object that will rotate with the mouse")]
+    [SerializeField] Transform cameraRotationPivot;
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("test move");
+    }
+
+    public void OnNext(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPrevious(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    void Start()
+    {
+        var inputActions = InputManager.Instance.inputActions;
+        inputActions.Player.Move.performed += OnMove;
+    }
+    
+
+}
