89e29e8e4fd5a6103ed9e120033aa7e2168f715b
Alinto <AntonLisborg@gmail.com>
2025-09-15 20:44:19 +0200

    yes

--- Assets/Scripts/Player/PlayerCharacterController.cs
+++ Assets/Scripts/Player/PlayerCharacterController.cs
@@ -6,39 +6,82 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerCharacterController : MonoBehaviour, ProjectWideInputActions.IPlayerActions
 {
-    [Tooltip("The object that will rotate with the mouse")]
-    [SerializeField] Transform cameraRotationPivot;
+    [Tooltip("The Transform that will rotate up and down with the mouse")]
+    [SerializeField] Transform cameraXRotationPivot;
+    [SerializeField] Vector3 gravity;
+    [SerializeField] float walkingSpeed = 1f;
+    [SerializeField] float runningSpeed = 1.5f;
+
+    CharacterController characterController;
+
+    Vector2 moveInputVector;
+
+    private float xCameraRotation;
+
+    private Vector3 velocity;
 
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
+
         var inputActions = InputManager.Instance.inputActions;
-        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.AddCallbacks(this);
+    }
+
+    void OnDisable()
+    {
+        var inputActions = InputManager.Instance.inputActions;
+        inputActions.Player.RemoveCallbacks(this);
     }
 
 
     void Update()
     {
+        velocity = Vector3.zero;
+
         ApplyGravity();
+        ApplyMovement();
+
+        characterController.Move(velocity * Time.deltaTime);
     }
 
     private void ApplyGravity()
     {
-        
+        velocity += gravity;
     }
 
     private void ApplyMovement()
     {
-        throw new NotImplementedException();
+        
+        velocity += Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f) *  new Vector3(moveInputVector.x, 0, moveInputVector.y) * walkingSpeed;
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        Debug.Log("character move");
+        moveInputVector = context.ReadValue<Vector2>();
+
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
-        throw new NotImplementedException();
+        RotateCamera(context.ReadValue<Vector2>());
+    }
+
+    void RotateCamera(Vector2 mouseDelta)
+    {
+        xCameraRotation -= mouseDelta.y;
+        xCameraRotation = Mathf.Clamp(xCameraRotation, -90f, 90f);
+
+        // Apply the rotation to the camera
+        cameraXRotationPivot.localRotation = Quaternion.Euler(xCameraRotation, 0f, 0f);
+        
+        // Rotate the player body
+        transform.Rotate(Vector3.up * mouseDelta.x);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        
     }
 
     public void OnAttack(InputAction.CallbackContext context)
@@ -52,11 +95,6 @@ public class PlayerCharacterController : MonoBehaviour, ProjectWideInputActions.
     }
 
     public void OnCrouch(InputAction.CallbackContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
     {
         throw new NotImplementedException();
     }
